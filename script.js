5a445078717160c6638d6a6ebff45a1e7333f414
matsub <matsub.rk@gmail.com>
2018-07-03 15:14:10 +0900

    deal with multiple tracks

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var pc1 = new RTCPeerConnection()
 var pc2 = new RTCPeerConnection()
 var pc1video = document.getElementById("pc1video")
+var pc1video2 = document.getElementById("pc1video2")
 var pc2video = document.getElementById("pc2video")
 
-var removingStream = null;
-var removingSender = null;
+var removingStream = null
+var removingSender = null
+var removingSender2 = null
 
 
 async function getExactVideoDevice(deviceId) {
@@ -19,11 +21,23 @@ async function getExactVideoDevice(deviceId) {
 pc1.ontrack = event => {
   pc2video.srcObject = event.streams[0]
 }
+
+// toggle two video element
+var videoToggle = true
 pc2.ontrack = event => {
   stream = event.streams[0]
-  pc1video.srcObject = stream
+
+  if (videoToggle) {
+    pc1video.srcObject = stream
+  } else {
+    pc1video2.srcObject = stream
+  }
+
+  videoToggle = !videoToggle
+  // set event "removetrack"
   stream.onremovetrack = e => console.info(e)
 }
+// set event "removestream"
 pc2.onremovestream = e => console.info(e)
 
 
@@ -40,7 +54,12 @@ pc2.onicecandidate = event => {
 }
 
 // negotiation
-pc1.onnegotiationneeded = async event => {
+async function nego (event) {
+  if (pc1.signalingState !== "stable") {
+    setTimeout(() => nego(event), 100)
+    return
+  }
+
   offer = await pc1.createOffer()
   await pc2.setRemoteDescription(offer)
   await pc1.setLocalDescription(offer)
@@ -52,13 +71,19 @@ pc1.onnegotiationneeded = async event => {
   await pc2.setLocalDescription(answer)
 }
 
+pc1.onnegotiationneeded = nego
+
 
 // connection
 async function call () {
   pc1stream = await getExactVideoDevice(1)
+  pc1stream2 = await getExactVideoDevice(2)
   pc1sender = pc1.addTrack(pc1stream.getTracks()[0], pc1stream)
+  pc1sender2 = pc1.addTrack(pc1stream2.getTracks()[0], pc1stream2)
+
   removingStream = pc1stream
   removingSender = pc1sender
+  removingSender2 = pc1sender2
 }
 
 function removeStream() {
@@ -69,11 +94,11 @@ function removeTrack() {
   pc1.removeTrack(removingSender)
 }
 
-function close() {
-  pc1.close()
+function removeTrack2() {
+  pc1.removeTrack(removingSender2)
 }
 
 document.getElementById("call").addEventListener("click", call)
 document.getElementById("removeStream").addEventListener("click", removeStream)
 document.getElementById("removeTrack").addEventListener("click", removeTrack)
-document.getElementById("close").addEventListener("click", close)
+document.getElementById("removeTrack2").addEventListener("click", removeTrack2)
